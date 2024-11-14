0eeb9d2408e5a6504e6c71d5d4399a0f12cea759
ThePirozhkov <igromansers@gmail.com>
2024-11-14 22:17:45 +0300

    COMMIT - Добавлен модуль Gateway - Все микросервисы связаны через Spring Cloud

--- Gateway/src/test/java/by/baby/gateway/GatewayApplicationTests.java
+++ Gateway/src/test/java/by/baby/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package by.baby.eurekaserver;
+package by.baby.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class GatewayApplicationTests {
 
     @Test
     void contextLoads() {
